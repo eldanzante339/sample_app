89c6304dac15dda0fa625f7113beeb3d834ba29d
Marvin Niederhaus <eldanzante339@aol.com>
2012-04-06 21:43:11 -0500

    "Initial Commit"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppNetbeans::Application.config.secret_token = '7f62cf8544245651883a54ea02ddd516f9fe166ed22e92ed60cd3292dfca211d57143f4c4bac795cfc69ff89bbb88006b5894997c40928c50ccc50ba0751f16c'
